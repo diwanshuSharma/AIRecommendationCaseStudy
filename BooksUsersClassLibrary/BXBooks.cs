ce6f79d7bcdac8f0c78d87b9b908c8da00c3284f
diwanshu <diwanshusharma23@gmail.com>
2020-12-16 11:53:12 +0530

    Entity class created again

--- BooksUsersClassLibrary/BXBooks.cs
+++ BooksUsersClassLibrary/BXBooks.cs
@@ -9,7 +9,7 @@ namespace BooksUsersClassLibrary
         public string ISBN { get; set; }
         public string BookTitle { get; set; }
         public string BookAuthor { get; set; }
-        public string YearOfPublication { get; set; }
+        public int YearOfPublication { get; set; }
         public string Publisher { get; set; }
         public string ImageURLS { get; set; }
         public string ImageURLM { get; set; }
