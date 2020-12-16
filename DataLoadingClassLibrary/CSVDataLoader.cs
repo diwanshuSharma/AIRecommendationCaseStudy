504c86f2ae9821da94aad8df924d8de2fccedf86
diwanshu <diwanshusharma23@gmail.com>
2020-12-16 13:56:34 +0530

    csv reader

--- DataLoadingClassLibrary/CSVDataLoader.cs
+++ DataLoadingClassLibrary/CSVDataLoader.cs
@@ -1,5 +1,8 @@
 ﻿using BooksUsersClassLibrary;
+using LumenWorks.Framework.IO.Csv;
 using System;
+using System.Data;
+using System.IO;
 
 namespace DataLoadingClassLibrary
 {
@@ -7,6 +10,16 @@ namespace DataLoadingClassLibrary
     {
         public BookDetails Load()
         {
+
+            var csvTable = new DataTable();
+
+            using (var csvReader = new CsvReader(new StreamReader(System.IO.File.OpenRead(@"C:\Eurofins-Training\RecommendationEngine_Project\BX-CSV-Dump\BX_Books.csv")), true))
+            {
+                csvTable.Load(csvReader);
+            }
+
+            Console.WriteLine(csvTable.ToString());
+
             throw new NotImplementedException();
         }
     }
